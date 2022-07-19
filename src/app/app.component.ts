523200b888ae665295052c671aa14aa5efe676e2
Mark Kamradt <mark.kamradt@gmail.com>
2022-07-18 20:25:19 -0700

    connected frontend to backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Content } from './models/content'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Agora';
-  greeting = { 'id': 'yugyfoog', 'content': 'Hello, World'};
+
+  greeting : Content = { 'id': '', 'message': '' };
+
+  constructor(private http: HttpClient) {
+    http.get<Content>('http://localhost:8080/resource')
+        .subscribe(data => this.greeting = data);
+  }
+
 }
---
+++ src/app/models/content.ts
@@ -0,0 +1,10 @@
+export class Content {
+  id: string;
+  message: string;
+
+  constructor(id: string, message: string) {
+    this.id = id;
+    this.message = message;
+  }
+
+}
